37afe0eb9efc0eb99962e1cf89c5174183dac537
ajiinkya <ajinkya.bhurake@gmail.com>
2023-12-29 12:05:05 +0530

    updated build image to v2

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -35,4 +35,33 @@ public class WeatherForecastController : ControllerBase
         })
         .ToArray();
     }
+
+    [HttpGet("{key}")]
+    public async Task<ActionResult<string>> CallThirdParty(string key)
+    {
+        try
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string url = $"http://10.244.0.41:5000/connect/{key}";
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return Ok($"Success for {key}.");
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return BadRequest();
+        }
+
+    }
 }
